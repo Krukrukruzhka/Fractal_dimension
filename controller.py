88305c5006de938e987ee396fc506aaad6287249
Krukrukruzhka <misha230503@mail.ru>
2022-12-01 14:19:11 +0300

    Доработаны функции Адаптирован под Singleton

--- controller.py
+++ controller.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtWidgets
 from PyQt5.QtGui import QPixmap
 import matplotlib.pyplot as plt
 import design
@@ -11,15 +11,16 @@ class LabApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.handler = model.HerstModel()
+        model.HerstModel.get_instance()
         self.timer = mytimer.MyTimer()
         self.timer.timeout.connect(self.display_plots)
-        self.progressBar.hide()  # Это нужно будет перенести в design.py в самом конце
+        self.progressBar.hide()  # replace to design.py
         self.pushButton.clicked.connect(self.browse_folder)
         self.pushButton_2.clicked.connect(self.calculate)
-        self.pushButton_3.clicked.connect(self.editFile)
-        self.pushButton_4.clicked.connect(self.hidePlots)
+        self.pushButton_3.clicked.connect(self.edit_file)
+        self.pushButton_4.clicked.connect(self.hide_plots)
 
-    def editFile(self):
+    def edit_file(self):
         self.__clear_plots()
         self.handler.kardio_list = []
         with open(f'Кардиограммы\\{self.handler.filename}', 'w+') as file:
@@ -33,9 +34,9 @@ class LabApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
                     s = s+i
             self.handler.kardio_list.append(float(s))
             file.writelines(list(map(lambda x: str(x)+'\n', self.handler.kardio_list)))
-        self.handler.herstCalculate()
+        self.handler.herst_calculate()
 
-    def hidePlots(self):
+    def hide_plots(self):
         count = 3
         if not self.checkBox.isChecked():
             count -= 1
@@ -89,13 +90,15 @@ class LabApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
             self.progressBar.hide()
             return
 
-    def create_scene(self, data, index: int, title: str, filename: str, width: int, height: int, with_tail: bool):
+    def create_scene(self, data, index: int, title: str, filename: str, width: int, height: int, with_tail: bool,
+                     y_title: str):
         plt.figure(figsize=(width, height))
         plt.xlim((-1, len(data)+1))
         plt.ylim((min(data)-0.2, max(data)+0.2))
         if with_tail:
             plt.plot(list(range(index - 1, len(data))), data[index-1:], color='red', linewidth=2)
         plt.plot(list(range(index)), data[:index], color='blue', linewidth=2)
+        plt.ylabel(y_title)
         plt.grid()
         plt.title(title)
         plt.savefig(filename)
@@ -117,19 +120,45 @@ class LabApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         elif not self.graphicsView_3.isHidden():
             width = self.graphicsView_3.width()//100 - 0.2
             height = self.graphicsView_3.height()//100 - 0.2
-        self.graphicsView.setScene(self.create_scene(self.handler.kardio_list, index+3, 'Кардиограмма',
-                                                         'fig1.png', width, height, True))
-        self.graphicsView_2.setScene(self.create_scene(self.handler.herst_list, index, 'Показатель Херста',
-                                                       'fig2.png', width, height, False))
-        self.graphicsView_3.setScene(self.create_scene(self.handler.dim_list, index, 'Размерность',
-                                                       'fig3.png', width, height, False))
+        plot_settings = {'data': self.handler.kardio_list,
+                         'index': index+3,
+                         'title': 'Кардиограмма',
+                         'filename': 'fig1.png',
+                         'width': width,
+                         'height': height,
+                         'with_tail': True,
+                         'y_title': ''
+                        }
+        self.graphicsView.setScene(self.create_scene(**plot_settings))
+
+        plot_settings = {'data': self.handler.herst_list,
+                         'index': index,
+                         'title': 'Показатели Херста в каждый момент кардиограммы',
+                         'filename': 'fig2.png',
+                         'width': width,
+                         'height': height,
+                         'with_tail': False,
+                         'y_title': 'Показатель Херста'
+                        }
+        self.graphicsView_2.setScene(self.create_scene(**plot_settings))
+
+        plot_settings = {'data': self.handler.dim_list,
+                         'index': index,
+                         'title': 'Стабильность ритма в каждый момент кардиограммы (0 - нестабильна, 1 - стабильна)',
+                         'filename': 'fig3.png',
+                         'width': width,
+                         'height': height,
+                         'with_tail': False,
+                         'y_title': 'Стабильность'
+                         }
+        self.graphicsView_3.setScene(self.create_scene(**plot_settings))
 
     def browse_folder(self):
         self.plainTextEdit.clear()
         self.__clear_plots()
         file_url = QtWidgets.QFileDialog.getOpenFileUrl(self, "Выберите файл", "Кардиограммы")
         filename = file_url[0].url().split('/')[9]
-        self.handler.loadData(filename=filename)
+        self.handler.load_data(filename=filename)
         self.label_3.setText(f"Текущий файл: {filename}")
         for point in self.handler.kardio_list:
             self.plainTextEdit.appendPlainText(str(point))
@@ -138,7 +167,7 @@ class LabApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def calculate(self):
         self.__clear_plots()
         self.__set_access(False)
-        self.handler.herstCalculate()
+        self.handler.herst_calculate()
         self.progressBar.show()
         self.progressBar.setMaximum(len(self.handler.herst_list))
-        self.timer.start(100)
+        self.timer.start(10)
