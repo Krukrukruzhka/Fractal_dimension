b1fa952277490b0b6e6352fc0322afd1ddaaa35d
Krukrukruzhka <misha230503@mail.ru>
2022-12-03 00:56:46 +0300

    Изменены названия переменных Добавлены строки для отладки

--- model.py
+++ model.py
@@ -1,39 +1,41 @@
 import math
 
 
-class HerstModel(object):
+class HurstModel(object):
     __instance = None
 
     def __init__(self):
         self.filename = ''
         self.kardio_list = []
-        self.herst_list = []
-        self.dim_list = []
-        if HerstModel.__instance:
+        self.hurst_list = []
+        self.res_list = []
+        if HurstModel.__instance:
             self.__instance = self.get_instance()
 
     @classmethod
     def get_instance(cls):
         if not cls.__instance:
-            cls.__instance = HerstModel()
+            cls.__instance = HurstModel()
         return cls.__instance
 
     def load_data(self, filename):
         self.filename = filename
-        link = f'Кардиограммы\\{filename}'  # Ссылка на файл
-        with open(link, 'r') as file:
+        path = f'Кардиограммы\\{filename}'  # Ссылка на файл
+        with open(path, 'r') as file:
             self.kardio_list = list(map(float, file.readlines()))
 
     @staticmethod
-    def herst(data, timepoint: int, coeff: float):
-        r = max(data[:timepoint]) - min(data[:timepoint])  # Размах
-        n = len(data[:timepoint])  # Кол-во элементов
-        mean = sum(data[:timepoint]) / n  # Среднее
-        std = (sum([(i - mean) ** 2 for i in data[:timepoint]]) / n) ** 0.5  # Стандартное отклонение
+    def hurst(time_series, timepoint: int, coeff: float):
+        r = max(time_series[:timepoint]) - min(time_series[:timepoint])  # Размах
+        n = len(time_series[:timepoint])  # Кол-во элементов
+        mean = sum(time_series[:timepoint]) / n  # Среднее
+        std = (sum([(i - mean) ** 2 for i in time_series[:timepoint]]) / n) ** 0.5  # Стандартное отклонение
         return math.log(r / std) / math.log(coeff * n)
 
-    def herst_calculate(self):
-        self.herst_list = []
+    def hurst_calculate(self):
+        print('hurst_calculate start')
+        self.hurst_list = []
         for i in range(4, len(self.kardio_list)+1):
-            self.herst_list.append(self.herst(data=self.kardio_list, timepoint=i, coeff=0.5))
-        self.dim_list = [2-i for i in self.herst_list]
+            self.hurst_list.append(self.hurst(time_series=self.kardio_list, timepoint=i, coeff=0.4))
+        self.res_list = [1 if i > 1 else i for i in self.hurst_list]
+        print('hurst_calculate stop')
