c77d117ea73420d4a6abb7df202a815007898816
Krukrukruzhka <misha230503@mail.ru>
2022-12-25 00:36:10 +0300

    Реализован примитивный обработчик исключений

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ import controller
 import sys
 from PyQt5 import QtWidgets
 
+
+def exception(type, value, traceback):
+    msg = QtWidgets.QMessageBox()
+    msg.setWindowTitle("Ошибка")
+    msg.setText(f'Ошибка: {type}, {value}')
+    msg.setIcon(QtWidgets.QMessageBox.Warning)
+    msg.exec_()
+
+
 if __name__ == '__main__':
+    sys.excepthook = exception
     app = QtWidgets.QApplication(sys.argv)
     window = controller.LabApp()
     window.show()
